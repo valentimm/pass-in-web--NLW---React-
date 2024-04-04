8e17453b137c575bed366054b1f5327de135e2f3
Matheus <meduardo.valentim@gmail.com>
2024-04-04 19:24:52 -0300

    feat: add components to main app & styled html

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-export function App() {
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
 
+export function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+      <Header />
+      <AttendeeList />
+    </div>
   )
 }
